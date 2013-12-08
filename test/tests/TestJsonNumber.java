621f40a97181db0098287c4eb47e0f115d7647fa
Jesper Öqvist <jesper@llbit.se>
2013-12-08 23:04:41 +0100

    Fixed floating point conversions
    fixes #2 (github)

--- test/tests/TestJsonNumber.java
+++ test/tests/TestJsonNumber.java
@@ -13,4 +13,19 @@ public class TestJsonNumber {
 		JsonNumber num = new JsonNumber(3.141592);
 		assertEquals(3.141592, num.doubleValue(0), 0.00001);
 	}
+	@Test
+	public void testDoubleFormat_2() {
+		JsonNumber num = new JsonNumber(Double.NEGATIVE_INFINITY);
+		assertEquals(Double.NEGATIVE_INFINITY, num.doubleValue(0), 0);
+	}
+	@Test
+	public void testDoubleFormat_3() {
+		JsonNumber num = new JsonNumber(Double.POSITIVE_INFINITY);
+		assertEquals(Double.POSITIVE_INFINITY, num.doubleValue(0), 0);
+	}
+	@Test
+	public void testDoubleFormat_4() {
+		JsonNumber num = new JsonNumber(Double.NaN);
+		assertEquals(Double.NaN, num.doubleValue(0), 0);
+	}
 }
